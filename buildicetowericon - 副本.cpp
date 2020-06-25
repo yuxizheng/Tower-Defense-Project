41d8054e04e6da25c9ed6299b0d38b88839d2abb
yuxizheng <zhengyuxi2000@163.com>
2020-06-25 23:34:17 +0800

    6/25/
    FINALE I HOPE

---
+++ buildicetowericon - 副本.cpp
@@ -0,0 +1,18 @@
+#include "buildicetowericon.h"
+#include "game.h"
+#include "icetower.h"
+
+extern Game * game;
+
+BuildIceTowerIcon::BuildIceTowerIcon(QGraphicsItem *parent){
+    setPixmap(QPixmap(":/icons/Resources/buildIceTowericon.jpg"));
+}
+
+void BuildIceTowerIcon::mousePressEvent(QGraphicsSceneMouseEvent* event){
+    soundEmit();
+    if (!game->build){
+        game->build = new IceTower();
+        game->setCursor(QString(":/tower/Resources/icetower.jpg"),QPoint(720,128));
+    }
+}
+
